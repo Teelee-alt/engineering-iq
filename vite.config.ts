329d78fd3e75e0912dc0a46d11233cdb9e99ce45
tinashelvurayai-dot <tinashelvurayai@gmail.com>
2026-05-29 04:21:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin";
-import viteReact from "@vitejs/plugin-react";
-import { vercelPreset } from "@tanstack/start-vercel";
+import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin';
+import { vercelPreset } from '@tanstack/start-vercel';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     tanstackStart({
-      server: { entry: "server" },
       preset: vercelPreset(),
+      server: { entry: 'server' }
     }),
     viteReact(),
+    tailwindcss(),
+    tsConfigPaths(),
   ],
 });
