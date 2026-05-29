5db6c179ecdbb50783b0f9f57afce305df694e00
tinashelvurayai-dot <tinashelvurayai@gmail.com>
2026-05-29 04:37:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from "nitro/vite";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
-  },
-  vite: {
-    plugins: [
-      nitro({
-        preset: "vercel",
-      }),
-    ],
+    server: {
+      entry: "server",
+    },
   },
 });
