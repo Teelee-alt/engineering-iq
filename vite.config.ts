1470caa4e83f693b2d381cc9648435bb62d7c45b
tinashelvurayai-dot <tinashelvurayai@gmail.com>
2026-05-29 04:06:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { vercelPreset } from "@tanstack/start-vercel";
 
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-    // Add the Vercel preset
-    preset: vercelPreset(),
   },
 });
